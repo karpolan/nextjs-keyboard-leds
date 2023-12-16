f367dcd65c846432057b42ab4eadb8a5a7012a72
Anton Karpenko <karpolan@karpolan.com>
2023-12-16 22:36:36 +0100

    fix: code review

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -2,6 +2,15 @@ import { FunctionComponent, SVGAttributes } from 'react';
 import { ICON_COLOR_NORMAL, ICON_SIZE } from '@/components/config';
 import { ICONS } from './icons';
 
+/**
+ * How to use:
+ * 1. Import all required React, MUI or other SVG icons into ./icons/index.tsx file.
+ * 2. Add icons with "unique lowercase names" into shared ICONS object. Lowercase is a must!
+ * 3. Use icons everywhere in the App by their names in <Icon icon="xxx" /> component
+ * Important: properties of ICONS object MUST be lowercase!
+ * Note: You can use camelCase or UPPERCASE in the <Icon icon="someIconByName" /> component
+ */
+
 export interface IconProps extends SVGAttributes<SVGElement> {
   color?: string;
   icon?: string;
@@ -11,7 +20,12 @@ export interface IconProps extends SVGAttributes<SVGElement> {
 
 /**
  * Renders SVG icon by given Icon name
- * @param {string} [icon] - name of the Icon to render
+ * @component Icon
+ * @param {string} [color] - color of the Icon to render, defaults to ICON_COLOR_NORMAL
+ * @param {string} [icon] - name of the Icon to render, defaults to "default"
+ * @param {string|number} [size] - size of the Icon to render, defaults to ICON_SIZE
+ * @param {object} [style] - CSS style to apply to the Icon
+ * @param {string} [title] - optional title of the Icon to render
  */
 const Icon: FunctionComponent<IconProps> = ({
   color = ICON_COLOR_NORMAL,
